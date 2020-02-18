f65d40458f5499d8b84b8d632d052a48884acfbf
8ka1alu <46132263+8ka1alu@users.noreply.github.com>
2020-02-18 22:11:56 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
-$(function() {
-    $('.navToggle').click(function() {
-        $(this).toggleClass('active');
- 
-        if ($(this).hasClass('active')) {
-            $('.globalMenuSp').addClass('active');
-        } else {
-            $('.globalMenuSp').removeClass('active');
-        }
-    });
-});
+$(function () {
+	
+	$('.zdo_drawer_button').click(function () {
+		$(this).toggleClass('active');
+		$('.zdo_drawer_bg').fadeToggle();
+		$('nav').toggleClass('open');
+	})
+	$('.zdo_drawer_bg').click(function () {
+		$(this).fadeOut();
+		$('.zdo_drawer_button').removeClass('active');
+		$('nav').removeClass('open');
+	});
+})
