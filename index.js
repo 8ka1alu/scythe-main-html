da787f07a8d71bead3015ab9224227f7711230e2
8ka1alu <46132263+8ka1alu@users.noreply.github.com>
2020-02-18 20:12:55 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,42 +1,21 @@
-// jQuery無しでナビゲーションを開閉する関数
-function navToggle() {
+(function(){
+  'use strict';
+  
+  var show = document.getElementById('show');
+  //#showを取得
+  var hide = document.getElementById('hide');
+  //#hideを取得
 
-  // 開閉ボタンを取得
-  var toggleBtn = document.getElementById('nav-toggle');
+  show.addEventListener('click',function(){
+    //showをクリックした時、、
+    document.body.className = 'menu-open';
+    //bodyに.menu-openをつける
+  });
 
-  // 開閉するナビゲーション本体を取得
-  var navView = document.getElementById('nav-list');
+    hide.addEventListener('click',function(){
+    //hideをクリックした時、、
+    document.body.className = '';
+    //bodyのクラスを外す
+  });
 
-  // 開閉ボタンの現在のクラスを取得
-  var toggleBtnClass = toggleBtn.getAttribute('class');
-
-  // 開閉ボタンのクラスで条件分岐
-  // 1. 開閉ボタンのクラスが「close」だったら
-  if(toggleBtnClass == 'nav-toggle-button close') {
-
-    // 閉じている状態のクラスを削除
-    toggleBtn.classList.remove('close');
-    navView.classList.remove('close');
-
-    // 開いている状態のクラスを付与
-    toggleBtn.classList.add('open');
-    navView.classList.add('open');
-
-  }
-
-  // 2. 開閉ボタンのクラスが「open」だったら
-  else {
-
-    // 開いている状態のクラスを削除
-    toggleBtn.classList.remove('open');
-    navView.classList.remove('open');
-
-    // 閉じている状態のクラスを付与
-    toggleBtn.classList.add('close');
-    navView.classList.add('close');
-  }
-
-}
-
-// 指定IDをクリックした際に関数を実行
-document.getElementById('nav-toggle').onclick = navToggle;
+})();
