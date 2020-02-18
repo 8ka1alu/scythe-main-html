c45c143cadbc7422256ab731ad5155871313a8e0
8ka1alu <46132263+8ka1alu@users.noreply.github.com>
2020-02-18 17:55:17 +0900

    Update and rename index.php to main.php

--- index.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-header('Location: /index.html');
-header('Location: /about.html');
-header('Location: /access.html');
-header('Location: /contact.html');
-header('Location: /menu.html');
-
-?>
