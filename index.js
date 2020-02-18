e0c3572f6fb380bbe1bd8282be382c7844796915
8ka1alu <46132263+8ka1alu@users.noreply.github.com>
2020-02-18 21:11:39 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,11 @@
 $(function() {
-  $("#navi-btn").click(function(){
-    if (!$("#navi-btn").hasClass("navi-btn-on")) {
-      $("#navi-btn").addClass("navi-btn-on");
-      $("#navi-btn p").text("CLOSE");
-      $("#navi-menu").fadeIn();
-    } else {
-      $("#navi-btn").removeClass("navi-btn-on")
-      $("#navi-btn p").text("MENU");
-      $("#navi-menu").fadeOut();
-    }
-  });
+    $('.navToggle').click(function() {
+        $(this).toggleClass('active');
+ 
+        if ($(this).hasClass('active')) {
+            $('.globalMenuSp').addClass('active');
+        } else {
+            $('.globalMenuSp').removeClass('active');
+        }
+    });
 });
